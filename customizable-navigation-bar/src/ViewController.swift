9b4639af86619b47890b40b5854fa26c43eb21e8
srfunksensei <sr.funk.sensei@gmail.com>
2019-07-25 16:08:45 +0200

    implementing setup of custom navigation bar methods

--- customizable-navigation-bar/src/ViewController.swift
+++ customizable-navigation-bar/src/ViewController.swift
@@ -1,10 +1,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+        
+        setupNavigationBarItems()
+    }
+    
+    private func setupNavigationBarItems() {
+        navigationItem.title = "Custom navigation bar"
+        
+        setupLeftNavigationBar()
+        setupRightNavigationBar()
+    }
+    
+    private func setupLeftNavigationBar() {
+        let lockButton = UIButton(type: .system)
+        lockButton.setImage(UIImage(named: "secure-cloud-50.png"), for: .normal)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: lockButton)
+    }
+    
+    private func setupRightNavigationBar() {
+        let rebelButton = UIButton(type: .system)
+        rebelButton.setImage(UIImage(named: "sw-rebel-48.png")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        let empireButton = UIButton(type: .system)
+        empireButton.setImage(UIImage(named: "sw-empire-48.png")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: rebelButton), UIBarButtonItem(customView: empireButton)]
     }
 }
 
